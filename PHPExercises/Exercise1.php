ac30562ca37e456cc26275c4ca7b4205bd702b45
M303User <M303User@m303.uc-bcf.edu.ph>
2025-03-06 16:03:49 +0800

    Added PHPExercises folder with Exercise 1

---
+++ PHPExercises/Exercise1.php
@@ -0,0 +1,14 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "The Sum is $sum. ";
+echo "The Difference is $difference. ";
+echo "The Product is $product. ";
+echo "The Quotient is $quotient. ";
+?>
