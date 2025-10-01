e17b72242a349e2b452e48dade241e1240cacb0c
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:11:37 +0800

    Added index.php for Laboratory Activity 2: PHP Exercises

---
+++ Lab2_PHPExercises/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab2: PHP Exercises</title>
+</head>
+<body>
+
+<h1>Lab2: PHP Exercises</h1>
+
+<ul>
+    <li><a href="intro.php">Introduce Yourself</a></li>
+    <li><a href="math.php">Simple Math</a></li>
+    <li><a href="rect.php">Area and Perimeter of a Rectangle</a></li>
+    <li><a href="tconv.php">Temperature Converter</a></li>
+    <li><a href="var.php">Swapping Variables</a></li>
+    <li><a href="scalc.php">Salary Calculator</a></li>
+    <li><a href="bcalc.php">BMI Calculator</a></li>
+    <li><a href="manip.php">String Manipulation</a></li>
+    <li><a href="sim.php">Bank Account Simulation</a></li>
+    <li><a href="grade.php">Simple Grading System</a></li>
+    <li><a href="cconv.php">Currency Converter</a></li>
+    <li><a href="est.php">Travel Cost Estimator</a></li>
+</ul>
+    
+</body>
+</html>
