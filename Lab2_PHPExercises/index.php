8836487eb0340d5a01bdebbed673a440808beac6
brenn-08 <anterobhea@gmail.com>
2025-10-04 12:34:03 +0800

    Added index.php with initial code for Laboratory Activity 2: PHP Exercises

--- Lab2_PHPExercises/index.php
+++ Lab2_PHPExercises/index.php
@@ -5,24 +5,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lab2: PHP Exercises</title>
 </head>
+<style>
+
+</style>
 <body>
 
 <h1>Lab2: PHP Exercises</h1>
 
-<ul>
-    <li><a href="intro.php">Introduce Yourself</a></li>
-    <li><a href="math.php">Simple Math</a></li>
-    <li><a href="rect.php">Area and Perimeter of a Rectangle</a></li>
-    <li><a href="tconv.php">Temperature Converter</a></li>
-    <li><a href="var.php">Swapping Variables</a></li>
-    <li><a href="scalc.php">Salary Calculator</a></li>
-    <li><a href="bcalc.php">BMI Calculator</a></li>
-    <li><a href="manip.php">String Manipulation</a></li>
-    <li><a href="sim.php">Bank Account Simulation</a></li>
-    <li><a href="grade.php">Simple Grading System</a></li>
-    <li><a href="cconv.php">Currency Converter</a></li>
-    <li><a href="est.php">Travel Cost Estimator</a></li>
-</ul>
-    
+<div class="comics">
+    <div class="button">
+        <img src="https://i0.wp.com/finalweapon.net/wp-content/uploads/2025/09/The-Fragrant-Flower-Blooms-With-Dignity-episode-1.webp?fit=1200%2C675&ssl=1" alt="photo">
+        <div class="caption">Introduce Yourself</div>
+        <div class="bubble">HELLO!</div>
+    </div>
+    <div class="button">
+        <img src="https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2025/06/The-Fragrant-Flower-Blooms-with-Dignity-main-pv-screenshot.png" alt="photo">
+        <div class="caption">Simple Math</div>
+        <div class="bubble">1+1?</div>
+    </div>
+    <div class="button">
+        <img src="https://fwmedia.fandomwire.com/wp-content/uploads/2025/09/08061423/The-Fragrant-Flower-Blooms-with-Dignity.jpg" alt="photo">
+        <div class="caption">Area and Perimeter of a Rectangle</div>
+        <div class="bubble">GET RECT!</div>
+    </div>
+    <div class="button">
+        <img src="https://staticg.sportskeeda.com/editor/2025/08/bc327-17548149638560-1920.jpg?w=640" alt="photo">
+        <div class="caption">Temperature Converter</div>
+        <div class="bubble">HOT!</div>
+    </div>
+    <div class="button">
+        <img src="https://inasianspaces.com/wp-content/uploads/2025/08/the-fragrant-flower-blooms-with-dignity-saku-and-usami.jpg?w=1200" alt="photo">
+        <div class="caption">Swapping Variables</div>
+        <div class="bubble">SWAP!</div>
+    </div>
+    <div class="button">
+        <img src="https://media.butwhytho.net/wp-content/uploads/2025/09/The-Fragrant-Flower-Blooms-with-Dignity-Episode-4-But-Why-Tho.jpg" alt="photo">
+        <div class="caption">Salary Calculator</div>
+        <div class="bubble">$$$</div>
+    </div>
+    <div class="button">
+        <img src="https://images.immediate.co.uk/production/volatile/sites/3/2025/09/fragrant-flower-857c410-e1756897165776.jpg?quality=90&resize=980,654" alt="photo">
+        <div class="caption">BMI Calculator</div>
+        <div class="bubble">SLAY!</div>
+    </div>
+    <div class="button">
+        <img src="https://images3.alphacoders.com/139/thumb-1920-1398229.png" alt="photo">
+        <div class="caption">String Manipulation</div>
+        <div class="bubble">LOL!</div>
+    </div>
+        <div class="button">
+        <img src="https://fwmedia.fandomwire.com/wp-content/uploads/2025/07/14044129/the-fragrant-flower-blooms-with-dignity-1.png?width=800&height=500&quality=85&sharpen=true&format=webp&aspect_ratio=800%3A500&crop=center" alt="photo">
+        <div class="caption">Bank Account Simulation</div>
+        <div class="bubble">CHA-CHING!</div>
+    </div>
+        <div class="button">
+        <img src="https://staticg.sportskeeda.com/editor/2025/07/30a88-17530086843653-1920.jpg" alt="photo">
+        <div class="caption">Simple Grading System</div>
+        <div class="bubble">A+!</div>
+    </div>
+        <div class="button">
+        <img src="https://arumjournal.com/wp-content/uploads/2025/07/screenshot-2025-07-20-at-10.31.21-am.png?w=1024&h=666&crop=1" alt="photo">
+        <div class="caption">Currency Converter</div>
+        <div class="bubble">WOW!</div>
+    </div>
+        <div class="button">
+        <img src="https://www.leisurebyte.com/wp-content/uploads/2025/08/The-Fragrant-Flower-Blooms-with-Dignity-Episode-7-Review-5-1024x576.jpg" alt="photo">
+        <div class="caption">Travel Cost Estimator</div>
+        <div class="bubble">GO!</div>
+    </div>
+</div> 
+
 </body>
 </html>
